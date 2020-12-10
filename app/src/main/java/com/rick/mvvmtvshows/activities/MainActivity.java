090fef91a39aec2346da9d706f1b797ec89e5497
Rick Ren <rick_ren@askey.com>
2020-12-10 18:38:11 +0800

    Android MVVM | TV Shows App | Tutorial #3 | Displaying TV Shows Using RecyclerView With Data Binding https://www.youtube.com/watch?v=hEcma_uOZ7Y&list=PLam6bY5NszYOUDKwe-6tVhb3zVevwbHiK&index=3

--- app/src/main/java/com/rick/mvvmtvshows/activities/MainActivity.java
+++ app/src/main/java/com/rick/mvvmtvshows/activities/MainActivity.java
@@ -1,30 +1,51 @@
 package com.rick.mvvmtvshows.activities;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.os.Bundle;
-import android.widget.Toast;
-
 import com.rick.mvvmtvshows.R;
+import com.rick.mvvmtvshows.adapters.TVShowsAdapter;
+import com.rick.mvvmtvshows.databinding.ActivityMainBinding;
+import com.rick.mvvmtvshows.model.TVShow;
 import com.rick.mvvmtvshows.viewmodels.MostPopularTVShowsViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding activityMainBinding;
     private MostPopularTVShowsViewModel viewModel;
+    private List<TVShow> tvShows = new ArrayList<>();
+    private TVShowsAdapter tvShowsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        doInitialization();
+    }
+
+    private void doInitialization() {
+        activityMainBinding.tvShowsRecyclerView.setHasFixedSize(true);
         viewModel = new ViewModelProvider(this).get(MostPopularTVShowsViewModel.class);
+        tvShowsAdapter = new TVShowsAdapter(tvShows);
+        activityMainBinding.tvShowsRecyclerView.setAdapter(tvShowsAdapter);
         getMostPopularTVShows();
     }
 
     private void getMostPopularTVShows() {
-        viewModel.getMostPopularTVShows(0).observe(this, mostPopularTVShowsResponse ->
-                Toast.makeText(getApplicationContext(),
-                        "Total Pages: " + mostPopularTVShowsResponse.getTotalPages(), Toast.LENGTH_SHORT).show()
+        activityMainBinding.setIsLoading(true);
+        viewModel.getMostPopularTVShows(0).observe(this, mostPopularTVShowsResponse -> {
+                    activityMainBinding.setIsLoading(false);
+                    if (mostPopularTVShowsResponse != null) {
+                        tvShows.addAll(mostPopularTVShowsResponse.getTvShows());
+                        tvShowsAdapter.notifyDataSetChanged();
+                    }
+                }
         );
     }
 }
---
+++ app/src/main/java/com/rick/mvvmtvshows/adapters/TVShowsAdapter.java
@@ -0,0 +1,63 @@
+package com.rick.mvvmtvshows.adapters;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rick.mvvmtvshows.R;
+import com.rick.mvvmtvshows.databinding.ItemContainerTvShowBinding;
+import com.rick.mvvmtvshows.model.TVShow;
+
+import java.util.List;
+
+public class TVShowsAdapter extends RecyclerView.Adapter<TVShowsAdapter.TVShowsViewHolder> {
+
+    private final List<TVShow> tvShows;
+    private LayoutInflater layoutInflater;
+
+    public TVShowsAdapter(List<TVShow> tvShows) {
+        this.tvShows = tvShows;
+    }
+
+    @NonNull
+    @Override
+    public TVShowsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (layoutInflater == null) {
+            layoutInflater = LayoutInflater.from(parent.getContext());
+        }
+
+        ItemContainerTvShowBinding tvShowBinding = DataBindingUtil.inflate(
+                layoutInflater, R.layout.item_container_tv_show, parent, false
+        );
+
+        return new TVShowsViewHolder(tvShowBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TVShowsViewHolder holder, int position) {
+        holder.bindTVShow(tvShows.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return tvShows.size();
+    }
+
+    static class TVShowsViewHolder extends RecyclerView.ViewHolder {
+
+        private final ItemContainerTvShowBinding itemContainerTvShowBinding;
+
+        public TVShowsViewHolder(@NonNull ItemContainerTvShowBinding itemContainerTvShowBinding) {
+            super(itemContainerTvShowBinding.getRoot());
+            this.itemContainerTvShowBinding = itemContainerTvShowBinding;
+        }
+
+        public void bindTVShow(TVShow tvShow) {
+            itemContainerTvShowBinding.setTvShow(tvShow);
+            itemContainerTvShowBinding.executePendingBindings();
+        }
+    }
+}
